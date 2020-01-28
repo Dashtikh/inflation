181ffaa658e12658df1e8e2501d4344ca9bcfc2d
Dashtikh <mohammaddashti.kh@gmail.com>
2020-01-28 11:08:16 +0330

    code completed

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    InfFunction infFunction = new InfFunction();
+        System.out.println(infFunction.inflation(infFunction.calculate1(2000,14000,6000,8600,2000),infFunction.calculate2(1000,7000,3000,4300,1000)));
+
+
+    }
+
+}
